dbb9c5be0bfdd8effb91094714d55428aa5a768f
Richard Wiseman <richard.wiseman@bt.com>
2017-12-22 12:02:00 +0000

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -15,10 +15,6 @@ moment.locale('en-gb');
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Something', amount: 1, createdAt: 2500 }));
-store.dispatch(addExpense({ description: 'Water bill', amount: 123, createdAt: 2000 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 234, createdAt: 3000 }));
-
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
